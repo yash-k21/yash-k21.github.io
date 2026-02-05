64ff70caaaf51a82849e660e26f3a6cbcb06673a
yash-k21 <141487393+yash-k21@users.noreply.github.com>
2026-02-05 09:52:52 +0000

    Deploying to gh-pages from @ yash-k21/yash-k21.github.io@b52f8442b0d20620ae9ab278ab0d4b7f52a32af1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,49 +37,49 @@ ninja.data = [{
               window.location.href = "/books/the_godfather.html";
             },},{id: "projects-portfolio-optimization-dashboard",
           title: 'Portfolio Optimization Dashboard',
-          description: "Interactive portfolio optimization using modern risk metrics",
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
             },},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project.html";
+              window.location.href = "/hidden/";
             },},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project.html";
+              window.location.href = "/hidden/";
             },},{id: "projects-project-4",
           title: 'project 4',
           description: "another without an image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project.html";
+              window.location.href = "/hidden/";
             },},{id: "projects-project-5",
           title: 'project 5',
           description: "a project with a background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
+              window.location.href = "/hidden/";
             },},{id: "projects-project-6",
           title: 'project 6',
           description: "a project with no image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project.html";
+              window.location.href = "/hidden/";
             },},{id: "projects-project-7",
           title: 'project 7',
           description: "with background image",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
+              window.location.href = "/hidden/";
             },},{id: "projects-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
+              window.location.href = "/hidden/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
+              window.location.href = "/hidden/";
             },},{
         id: 'social-email',
         title: 'email',
